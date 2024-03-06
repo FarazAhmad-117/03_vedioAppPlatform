93a78dfa2bf6a7466105c774866dbd7507a786da
FarazAhmad-117 <su92-bscsm-s23-117@superior.edu.pk>
2024-03-06 18:36:49 +0500

    connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("Hello");
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: "./.env"
+});
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import express from "express";
+const app = express();
+;(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on('error',(error)=>{
+            console.log('Database is unable to connect with the database',error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is listening on port https://localhost:${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR::CONNECTING DATABASE",error);
+        throw error;
+    }
+})()
+
+*/
